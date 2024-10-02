d62de7e355ec26d69a83257234f7e6d15ab94d3c
atmak <atmakurunithesh911@gmail.com>
2024-10-02 20:41:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+function fibonacci(n) {
+    if (n <= 1)
+        return n;
+    let fibo = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        // calculate the next fibonacci number
+        // update the last two fibonacci numbers
+    }
+    return fibo[n];
+}
+
 }
 
 module.exports = fibonacci;
